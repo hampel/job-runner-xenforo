10a97cb393a9b3f05f7808247397b7132917e0c4
Simon Hampel <simon@hampelgroup.com>
2019-11-28 11:25:54 +1100

    Cli command and test job for testing purposes

---
+++ Cli/Command/TestJobs.php
@@ -0,0 +1,29 @@
+<?php namespace Hampel\JobRunner\Cli\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestJobs extends Command
+{
+	protected function configure()
+	{
+		$this
+			->setName('xf:test-jobs')
+			->setDescription('Generate a test job')
+			->addArgument(
+				'email',
+				InputArgument::REQUIRED,
+				"Email address to sent test message to"
+			);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$app = \XF::app();
+		$app->jobManager()->enqueue("Hampel\JobRunner:TestJob", ['email' => $input->getArgument('email')]);
+
+		return 1;
+	}
+}
---
+++ Job/TestJob.php
@@ -0,0 +1,48 @@
+<?php namespace Hampel\JobRunner\Job;
+
+use Hampel\JobRunner\Cli\Logger;
+use XF\Job\AbstractJob;
+
+class TestJob extends AbstractJob
+{
+	public function run($maxRunTime)
+	{
+		$this->log("About to start test job", $this->data);
+
+		$mail = $this->app->mailer()->newMail();
+		$mail->setTo($this->data['email']);
+		$mail->setContent(
+			"Test job",
+			"This is an email sent from a test job"
+		);
+		$sent = $mail->send();
+
+		$this->log("Sent mail", ['sent' => $sent]);
+
+		return $this->complete();
+	}
+
+	public function getStatusMessage()
+	{
+		return "Testing jobs";
+	}
+
+	public function canCancel()
+	{
+		return false;
+	}
+
+	public function canTriggerByChoice()
+	{
+		return false;
+	}
+
+	protected function log($message, array $context = [])
+	{
+		if (!isset($this->app['cli.logger'])) return;
+
+		/** @var Logger $logger */
+		$logger = $this->app['cli.logger'];
+		$logger->logJobProgress($message, $context, $this);
+	}
+}
