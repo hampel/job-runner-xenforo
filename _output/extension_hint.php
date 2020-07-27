7552bc9cdb00780dcf89a9e8336bfba7342ac2d2
Simon Hampel <simon@hampelgroup.com>
2020-07-27 17:51:35 +1000

    converting to using master branch for everything - remove _output from gitignore; remove _data; add _output

---
+++ _output/extension_hint.php
@@ -0,0 +1,15 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace Hampel\JobRunner\XF\Job
+{
+	class XFCP_Cron extends \XF\Job\Cron {}
+	class XFCP_Manager extends \XF\Job\Manager {}
+}
+
+namespace Hampel\JobRunner\XF\Mvc\Renderer
+{
+	class XFCP_Json extends \XF\Mvc\Renderer\Json {}
+}
