484d87febe6a320435ffc059e6f5b13162a16e99
Simon Hampel <simon@hampelgroup.com>
2020-08-29 08:15:53 +1000

    prevent JobRunner 1.x from being installed on XenForo 2.2

---
+++ Setup.php
@@ -0,0 +1,38 @@
+<?php namespace Hampel\JobRunner;
+
+use XF\AddOn\AbstractSetup;
+use XF\Db\Schema\Create;
+
+class Setup extends AbstractSetup
+{
+	public function install(array $stepParams = [])
+	{
+		// Nothing to do
+	}
+
+	public function upgrade(array $stepParams = [])
+	{
+		// Nothing to do
+	}
+
+	public function uninstall(array $stepParams = [])
+	{
+		// Nothing to do
+	}
+
+	/**
+	 * Perform additional requirement checks.
+	 *
+	 * @param array $errors Errors will block the setup from continuing
+	 * @param array $warnings Warnings will be displayed but allow the user to continue setup
+	 *
+	 * @return void
+	 */
+	public function checkRequirements(&$errors = [], &$warnings = [])
+	{
+		if (\XF::$version >= 2020000)
+		{
+			$errors[] = 'This version of Hampel/JobRunner is not compatible with XenForo v2.2 - please upgrade to v2.x of the JobRunner addon';
+		}
+	}
+}
